1fa08286e7e6b55b580b6f05438985a2c0c305af
Miehau <michal.mlak.1@gmail.com>
2024-10-27 16:52:38 +0000

    Make layout common

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,9 @@
-<script>import "../app.css";</script><slot></slot>
+<script lang="ts">
+    import "../app.css";
+    import MainLayout from "$lib/components/MainLayout.svelte";
+</script>
+
+<!-- Add data-sveltekit-preload-data and data-sveltekit-reload="off" to the root element -->
+<MainLayout>
+    <slot />
+</MainLayout>
